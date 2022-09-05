ab20ffcbff86c0345bcaa9d251388ca69d7b794d
Taveira <62896500+TaveiraTavel@users.noreply.github.com>
2022-09-05 10:40:17 -0300

    Implementando Splash Screen

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        },3500);
     }
 }
