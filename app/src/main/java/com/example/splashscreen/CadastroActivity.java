ea89f28fca1f6de2f573d25830e58742544d7e88
Taveira <62896500+TaveiraTavel@users.noreply.github.com>
2022-10-03 10:18:50 -0300

    Concluindo cadastro e login

--- app/src/main/java/com/example/splashscreen/CadastroActivity.java
+++ app/src/main/java/com/example/splashscreen/CadastroActivity.java
@@ -2,14 +2,50 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.splashscreen.usuario.Usuario;
+import com.example.splashscreen.usuario.UsuarioDAO;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    Button btnConcluir;
+    EditText txtNome, txtLogin, txtSenha;
+    String UsuarioNome, UsuarioLogin, UsuarioSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_cadastro);
+
+        btnConcluir = findViewById(R.id.btnCadastrar);
+        txtNome = findViewById(R.id.editTextNome);
+        txtLogin = findViewById(R.id.editTextEmail);
+        txtSenha = findViewById(R.id.editTextSenha);
+
+        Intent intent = getIntent();
+
+        btnConcluir.setOnClickListener(v -> {
+            UsuarioNome = txtNome.getText().toString();
+            UsuarioLogin = txtLogin.getText().toString();
+            UsuarioSenha = txtSenha.getText().toString();
+
+            Usuario usuario = new Usuario(UsuarioLogin, UsuarioSenha, UsuarioNome);
+
+            UsuarioDAO usuarioDAO = new UsuarioDAO(CadastroActivity.this);
+
+            try {
+                usuarioDAO.cadastrarUsuario(usuario);
+                Toast.makeText(getApplicationContext(), "Cadastro efetuado com sucesso", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/splashscreen/LoginActivity.java
+++ app/src/main/java/com/example/splashscreen/LoginActivity.java
@@ -1,15 +1,32 @@
 package com.example.splashscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import com.example.splashscreen.usuario.Usuario;
+import com.example.splashscreen.usuario.UsuarioDAO;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button btnCadastro;
+    private static final String FILE_NAME = "usuarioLogado.json";
+    private EditText userLogin, userSenha;
+    private Button btnLogin;
+    private Usuario usuario;
+    private UsuarioDAO usuarioDAO;
+    private Button btnCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +34,95 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
+
+        userLogin = findViewById(R.id.txtEmailLogin);
+        userSenha = findViewById(R.id.txtSenhaLogin);
+        btnLogin = findViewById(R.id.btnentrar);
         btnCadastro = findViewById(R.id.btncadastro);
+
         btnCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent main = new Intent(getApplicationContext(), CadastroActivity.class);
-                startActivity(main);
+                Intent cadastro = new Intent(getApplicationContext(), CadastroActivity.class);
+                startActivity(cadastro);
             }
         });
 
+        btnLogin.setOnClickListener(v ->{
+            String login = String.valueOf(userLogin.getText());
+            String senha = String.valueOf(userSenha.getText());
+
+            validarCampos();
+
+            usuarioDAO = new UsuarioDAO(getApplicationContext());
+
+            if (usuarioDAO.verificarLogin(login, senha)){
+                Usuario usuario = usuarioDAO.buscarUsuario(login);
+
+                Gson gson = new Gson();
+                String json = gson.toJson(usuario);
+                gravarDados(json);
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+            else{
+                Toast.makeText(LoginActivity.this, "Usuário ou senha não correspondem", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+    // ARMAZENAR DADOS
+    private void gravarDados(String json) {
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(json.getBytes());
+            Toast.makeText(this, "Usuário logado.", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // VALIDAR CAMPOS
+    private void validarCampos(){
+        boolean verificacao = false;
+
+        String email = userLogin.getText().toString();
+        String senha = userSenha.getText().toString();
+
+        if (verificacao = campoNulo(email)) {
+            userLogin.requestFocus();
+            Toast.makeText(this, "Preencha o campo e-mail.", Toast.LENGTH_SHORT).show();
+        } else if (verificacao = campoNulo(senha)) {
+            userSenha.requestFocus();
+            Toast.makeText(this, "Preencha o campo senha.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean campoNulo (String campo){
+        boolean verificacao = (TextUtils.isEmpty(campo) || campo.trim().isEmpty());
+        return verificacao;
+    }
+
+    // SAVED INSTANCE
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String email = userLogin.getText().toString();
+        outState.putString("Email", email);
     }
 
 
