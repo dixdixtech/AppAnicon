1e978660c023f01f9f4e6a8cde297fa0d20aaec0
Levi <dslevifilho@outlook.com>
2022-11-11 10:06:19 -0300

    Adicionando funcionalidades para o botão de perfil

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton btnPerfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        btnPerfil = findViewById(R.id.imageButtonPerfil);
+        btnPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }
