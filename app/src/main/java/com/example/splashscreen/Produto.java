7209d624156b1943399fadeacde9b6e8038e386f
Taveira <62896500+TaveiraTavel@users.noreply.github.com>
2022-11-23 08:39:05 -0300

    Adicionando a classe produto

---
+++ app/src/main/java/com/example/splashscreen/Produto.java
@@ -0,0 +1,75 @@
+package com.example.splashscreen;
+
+public class Produto {
+
+    private String id;
+    private String cat;
+    private String name;
+    private String garantia;
+    private String validade;
+    private String qtdestoque;
+    private String desc;
+
+
+    public String getId() {
+        return id;
+    }
+     public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getGarantia() {
+        return garantia;
+    }
+
+    public void setGarantia(String garantia) {
+        this.garantia = garantia;
+    }
+
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+
+    public String getQtdestoque() {
+        return qtdestoque;
+    }
+
+    public void setQtdestoque(String qtdestoque) {
+        this.qtdestoque = qtdestoque;
+    }
+
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
+
