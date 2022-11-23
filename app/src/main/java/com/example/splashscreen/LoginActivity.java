f011d8dd971591455b06a48812d8cf956059c2e9
3etimds <3etimds@LAB3-19>
2022-11-23 08:04:45 -0300

    Adicionando Categorias e consertando os erros

--- app/src/main/java/com/example/splashscreen/LoginActivity.java
+++ app/src/main/java/com/example/splashscreen/LoginActivity.java
@@ -35,10 +35,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
         setContentView(R.layout.activity_login);
 
 
